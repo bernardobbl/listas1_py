63f5abfba3e941bd447c80d6ee74f60736b75dab
Bernardo Barcellos Leite <bernardobarcellosleite@gmail.com>
2023-05-29 11:22:58 -0300

    primeiro commit

---
+++ lista_tuplas/listaT1.py
@@ -0,0 +1,11 @@
+pessoa = {
+    "primeiro_nome": "Bernardo",
+    "sobrenome": "Barcellos Leite",
+    "idade": 18,
+    "cidade": "Nova Lima - MG"
+}
+
+print("Primeiro Nome:", pessoa["primeiro_nome"])
+print("Sobrenome:", pessoa["sobrenome"])
+print("Idade:", pessoa["idade"])
+print("Cidade:", pessoa["cidade"])
---
+++ lista_tuplas/listaT2.py
@@ -0,0 +1,12 @@
+# Questão 2 - Dicionário de números favoritos
+
+numeros_favoritos = {
+    "Lucas": 20,
+    "Bernardo": 13,
+    "André": 98,
+    "Roberto": 15,
+    "Ian": 10
+}
+
+for pessoa, numero in numeros_favoritos.items():
+    print(pessoa + ":", numero)
